d002109a6d1331c10fe5c7b2dbf9cab7d040ae37
Oleg Khutorianskiy <compilated510@outlook.com>
2022-10-16 00:45:56 +0200

    task lvl 1, more arrows func, explanations

---
+++ Work_Home/WH_12.10/script.js
@@ -0,0 +1,46 @@
+//Function Expression
+
+// const string = function (str) {
+
+//     if ((typeof str) === 'string') return str.length;
+//     if ((typeof str) === 'boolean') return (String(str).length);
+//     if (str == 0) return 'zero';
+//     if (str == 1) return 'one';
+//     if (str == 2) return 'two';
+//     if (str == 3) return 'three';
+//     if (str == 4) return 'fore'; 
+//             return 'Неверный тип данных';
+            
+//     }
+// console.log(string({}));
+
+//Function Declaration
+
+// function string(str) {
+//     if ((typeof str) === 'string') {
+//         return str.length;
+//     }
+//     if ((typeof str) === 'boolean') {
+//         return (String(str).length);
+//     }
+//     if (str == 0) {
+//         return 'zero';
+//     }
+//     if (str == 1) {
+//         return 'one';
+//     }
+//     if (str == 2) {
+//         return 'two';
+//     }
+//     if (str == 3) {
+//         return 'three';
+//     }
+//     if (str == 4) {
+//         return 'fore';
+//     } 
+//             return 'Неверный тип данных';
+// }
+// console.log(string(0===0));
+
+// Arrows Function
+
