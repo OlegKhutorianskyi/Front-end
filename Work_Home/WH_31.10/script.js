9b5a341a6f4da725eddc3e533cf96e1362157059
Oleg Khutorianskiy <compilated510@outlook.com>
2022-11-04 01:04:38 +0100

    noDone

--- Work_Home/WH_31.10/script.js
+++ Work_Home/WH_31.10/script.js
@@ -2,16 +2,13 @@
 // TODO _ Напишите функцию count(obj), которая возвращает количество свойств объекта:
 
 
-let user = {
-    name: 'John',
-    age: 30,
-    "John": 500,
-    "Mary": 550
-
-
- };
- let count = obj => {let objLenth = Object.keys(obj).length; return objLenth;}
- console.log(count(user));
+// let user = {
+//     name: 'John',
+//     age: 30,
+//     "John": 500,
+//     "Mary": 550
+//  };
+//  let count = obj => Object.keys(obj).length;
 
 
 
@@ -19,24 +16,25 @@ let user = {
 // TODO _ Если объект salaries пустой, то нужно вернуть null.
 
  let salaries = {
-    "John": 500,
-    "Pete": 300,
-    "Mary": 550
+   "Pete": 2800,
+    "John": 3700,
+    "Mary": 1550
  };
  
 let topSalary = (obj) => {
-  if (obj.John > obj.Pete && obj.John > obj.Mary) {
-    obj.name = "John";
-    return obj.name;
-  } else if (obj.Pete > obj.John && obj.Pete > obj.Mary) {
-    obj.name = "Pete";
-    return obj.name;
-  } else if (obj.Mary > obj.John && obj.Mary > obj.Pete) {
-    obj.name = "Mary";
-    return obj.name;
-  } else {
-    return null;
+  let flag = 0;
+  for (let key in obj) {
+    if (obj[key] >= flag) {
+      flag = obj[key];
+    };
   }
+  return flag;
+
+
 };
 
 console.log(topSalary(salaries));
+
+//смог решить до этапа определения и вывода самого высокого числа
+
+
